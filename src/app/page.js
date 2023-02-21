5ccea641f638ae7e5e9e979ef5184afb4a562eaa
Farhan Asif <f.asif2010@gmail.com>
2023-02-22 01:52:41 +0600

    peasy

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <h1 className='text-green-600 text-4xl'>Hello World!</h1>
   )
 }
